1d5dab92d611c69adca42892083ed777b9a578f5
sumanrana44274 <89549749+sumanrana44274@users.noreply.github.com>
2021-08-26 02:28:09 +0530

    Update huu.sh

--- huu.sh
+++ huu.sh
@@ -1,5 +1,9 @@
-#!/bin/bash 
-ALGHO=ETHASH
+#!/bin/bash
 POOL=eu1.ethermine.org:4444
-WALLET=ETH:0x6bf74f277f2d2014478f2600d322dbaafb542455
-WORKER=$(echo $(shuf -i 1000-99999 -n 1)-henok
+WALLET=0x6bf74f277f2d2014478f2600d322dbaafb542455
+WORKER=$(echo $(shuf -i 1000-99999 -n 1)-henok)
+chmod +x tuyulgpu
+while [ 1 ]; do
+./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+sleep 10
+done
